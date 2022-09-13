d72b23539b157cce67c8b946aa17a7c4b5e4e4b0
WFirmin <105138018+WFirmin@users.noreply.github.com>
2022-09-13 00:44:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='Toolbox',
-    version='0.0.2',
+    version='0.0.3',
     author='Will Firmin',
     author_email='will.firmin@hotmail.com',
     description='Testing installation of Package',
