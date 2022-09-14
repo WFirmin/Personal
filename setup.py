5f9a3087fc82801c9a9d1417eb4981c554babb4b
WFirmin <105138018+WFirmin@users.noreply.github.com>
2022-09-13 21:40:11 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='WFirmin',
+    version='0.0.10',
+    author='Will Firmin',
+    author_email='will.firmin@hotmail.com',
+    description='Personal Toolbox',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/WFirmin/Personal',
+    license='MIT',
+    packages=['WFirmin'],
+    install_requires=["numpy","pandas","scipy"],
+)
