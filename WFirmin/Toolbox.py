f10eee02e33f4e81e9853d580f21e076d22cbf8b
WFirmin <105138018+WFirmin@users.noreply.github.com>
2022-09-13 21:37:39 -0700

    Added custom model feature

--- WFirmin/Toolbox.py
+++ WFirmin/Toolbox.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
+
+# Misc. Tools:
 
 def cut(v, n, unique=True, label="median", r=2):
   v = np.array(v)
@@ -33,3 +36,51 @@ def cut(v, n, unique=True, label="median", r=2):
       q[mask] = method((left,right))
   
   return q
+
+
+
+
+
+# Modeling:
+
+def logit(v):
+  v = np.array(v)
+  return 1 / (np.e**(-v) + 1)
+
+def MSE(true, pred):
+  true = np.array(true)
+  pred = np.array(pred)
+  return ((true - pred)**2).mean()
+
+# Example formula, uses b for betas and v for variable columns (dictionary or dataframe)
+example_formula = "lambda b,v: (b[0] + v['x1']) * logit(b[1] + v['x2'])"
+    
+class Model:
+  def __init__(self, dependent, formula, n_betas, betas=None):
+    if not betas == None: 
+      self.betas = betas
+    else: 
+      self.betas = [0] * n_betas
+    self.formula = formula
+    self.n_betas = n_betas
+    self.dependent = dependent
+    
+  def predict(self, data, betas=None):
+    # Data comes from a dictionary or dataframe, with columns that match those in the formula
+    if betas is None:
+      return self.formula(self.betas, data)
+    else:
+      return self.formula(betas, data)
+  
+  def fit(self, data, loss=MSE, warm_start=False, print_results=False):
+    # loss(true, pred) is the loss function to minimize
+    # define the function to optimize
+    optimization_function = lambda b: loss(data[self.dependent], self.predict(data, b))
+    # optimize
+    if warm_start: 
+      res = minimize(optimization_function, x0=self.betas)
+    else: 
+      res = minimize(optimization_function, x0=[0]*self.n_betas)
+    if print_results: print(res)
+    self.betas = res.x
+    
