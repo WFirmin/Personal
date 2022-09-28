99bdf216f25e73529f5cabfe29bd72cd4865a9e9
Will Firmin <105138018+WFirmin@users.noreply.github.com>
2022-09-27 21:02:03 -0700

    Update __init__.py
    Updated imports so that WFirmin.Toolbox.MSE and WFirmin.Toolbox.logit can be used inside of WFirmin.Toolbox.Model

--- WFirmin/__init__.py
+++ WFirmin/__init__.py
@@ -1,2 +1,8 @@
 from WFirmin import Toolbox
+from WFirmin.Toolbox import logit
+from WFirmin.Toolbox import MSE
+from WFirmin.Toolbox import cut
+from WFirmin.Toolbox import Model
+
 from WFirmin import Visuals
+from WFirmin.Visuals import gradient
