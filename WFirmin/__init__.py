f1204cafdf215fa6e236ea1301a3c7a33d7d1e0a
Will Firmin <105138018+WFirmin@users.noreply.github.com>
2022-09-29 21:00:45 -0700

    Update __init__.py

--- WFirmin/__init__.py
+++ WFirmin/__init__.py
@@ -2,6 +2,9 @@ from WFirmin import Toolbox
 from WFirmin.Toolbox import logit
 from WFirmin.Toolbox import MSE
 from WFirmin.Toolbox import cut
+from WFirmin.Toolbox import concatList
+from WFirmin.Toolbox import Resample
+from WFirmin.Toolbox import elasticNet
 from WFirmin.Toolbox import Model
 
 from WFirmin import Visuals
