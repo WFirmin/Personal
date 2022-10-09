3ddd92c9f1d8023be0b4762d2d1b1a8ac75056d7
Will Firmin <105138018+WFirmin@users.noreply.github.com>
2022-10-08 18:51:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='WFirmin',
-    version='0.1.0',
+    version='0.1.1',
     author='Will Firmin',
     author_email='will.firmin@hotmail.com',
     description='Personal Toolbox',
